694d0ae8cd46ec4cbb9ca5a1bb3490c36a0f5ef6
NicolisAlessandro <73544633+NicolisAlessandro@users.noreply.github.com>
2022-10-19 10:36:35 +0200

    Update Grafica.java

--- src/nicolis_A_RFC/bin/Grafica.java
+++ src/nicolis_A_RFC/bin/Grafica.java
@@ -1,40 +1,35 @@
 package nicolis_A_RFC.bin;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
 
-public class Grafica extends JPanel implements WindowPanel {
+public class Grafica extends JPanel {
 
-        private Image image;
-        private final int panelWidth = 400;
-        private final int panelHeight = 450;
+    private Image image;
+    private final int panelWidth = 400;
+    private final int panelHeight = 450;
 
-        public Grafica() {
+    public Grafica() {
 
-            setLayout(new GridLayout(4, 0, 10, 0));
+        setLayout(new GridLayout(4, 0, 10, 0));
 
-            JLabel title = new JLabel("Marconi time system");
-            title.setHorizontalAlignment( SwingConstants.CENTER );
-            title.setForeground(Color.WHITE);
-            title.setFont(new Font("Impact", Font.PLAIN, 30));
+        JLabel title = new JLabel("Marconi time system");
+        title.setHorizontalAlignment(SwingConstants.CENTER);
+        title.setForeground(Color.WHITE);
+        title.setFont(new Font("Impact", Font.PLAIN, 30));
 
-            add(title, 0, 0);
+        add(title, 0, 0);
 
-        }
+    }
 
-        @Override
-        public Dimension getPreferredSize() {
-            return new Dimension(panelWidth, panelHeight);
-        }
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(panelWidth, panelHeight);
+    }
 
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-            g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
-        }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
     }
 }
